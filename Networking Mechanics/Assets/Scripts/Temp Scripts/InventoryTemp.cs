a35762eb110b5c86f2953ecf0064761b28da3847
lovesalt <claireleongax@gmail.com>
2020-05-16 17:22:46 +0800

    Implemented first draft of drop item mechanics
    Improved pickup mechanic and implemented drop item mechanics, however, player is unable to pick up items immediately after dropping them without having to exit and reenter the trigger

--- Networking Mechanics/Assets/Scripts/Temp Scripts/InventoryTemp.cs
+++ Networking Mechanics/Assets/Scripts/Temp Scripts/InventoryTemp.cs
@@ -9,15 +9,6 @@ public class InventoryTemp : MonoBehaviour
     public bool[] isFull; //check if there is already an item inside the slot(s)
     public GameObject[] slots; //assign the item we pick up to the inventory slot
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
---
+++ Networking Mechanics/Assets/Scripts/Temp Scripts/SpawnItemTemp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attached to item icon 
+//Spawns item at player position when dropped
+public class SpawnItemTemp : MonoBehaviour
+{
+    public GameObject droppedItem;
+
+    public void SpawnDroppedItem()
+    {
+        //Spawn dropped item
+        Debug.Log("Dropped an item");
+        Instantiate(droppedItem, PlayerMovementTest.playerPos, Quaternion.identity);
+        Debug.Log("Instantiated at" + PlayerMovementTest.playerPos);
+    }
+}
