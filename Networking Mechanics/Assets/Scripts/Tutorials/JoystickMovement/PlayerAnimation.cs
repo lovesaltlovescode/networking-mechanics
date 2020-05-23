097cf033f94bf1964aa917495856101e7ba138c6
lovesalt <claireleongax@gmail.com>
2020-05-23 13:06:05 +0800

    Made a movement tutorial
    Imported in Mix and Jam's Jammo for a movement tutorial using joystick

---
+++ Networking Mechanics/Assets/Scripts/Tutorials/JoystickMovement/PlayerAnimation.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Get animator component and set bools for anim to play
+
+public class PlayerAnimation : MonoBehaviour
+{
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();   
+    }
+
+    //function to move player
+    public void Move(float blend)
+    {
+        anim.SetFloat("Blend", blend);
+    }
+
+    //function make player happy
+    public void Jump(bool jump)
+    {
+        anim.SetBool("hhh", jump);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(anim.GetFloat("Blend"));
+    }
+}
---
+++ Networking Mechanics/Assets/Scripts/Tutorials/JoystickMovement/PlayerMovementTutorial.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using UnityEngine.UI;
+
+//move player, let player use interact button
+//integrate pickup items script
+//if collided item is an object, pick up by pressing the button
+//do the check if inventory is full when pressed the button
+//if inventory full, debug log
+//if inventory empty, then pick up and instantiate the respective icon
+public class PlayerMovementTutorial : MonoBehaviour
+{
+    private Rigidbody myBody;
+    public float moveForce = 25f;
+
+    public static Vector3 playerPos; //position of player
+
+    [SerializeField] private FixedJoystick joystick = null;
+
+    private PlayerAnimation anim;
+
+
+    void Start()
+    {
+        myBody = GetComponent<Rigidbody>();
+        anim = gameObject.GetComponent<PlayerAnimation>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+
+    }
+
+    void Move()
+    {
+        //playerPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 2, gameObject.transform.position.z);
+
+        //MOVE CHARACTER
+
+        myBody.velocity = new Vector3(joystick.Horizontal * moveForce, myBody.velocity.y, joystick.Vertical * moveForce); //x, y, z
+
+        //print(joystick.Horizontal);
+
+        //Pressing the joystick at one value at least
+        if (joystick.Horizontal != 0f || joystick.Vertical != 0f)
+        {
+            anim.Move(0.6f); //Run state
+
+            // Debug.Log("Joystick Horizontal: " + joystick.Horizontal);
+            // Debug.Log("Joystick Vertical: " + joystick.Vertical);
+
+            //Makes gameobject look towards direction specified, in this case, the movement direction of the player
+            transform.rotation = Quaternion.LookRotation(myBody.velocity);
+        }
+
+        else
+        {
+            anim.Move(0f); //Idle state   
+        }
+    }
+
+
+}
