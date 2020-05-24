d62f250ca528de12b3b6de6db6a46ef6b17e70fe
lovesalt <claireleongax@gmail.com>
2020-05-24 21:59:05 +0800

    Finished making joystick movement tutorial

--- Networking Mechanics/Assets/Scripts/Tutorials/JoystickMovement/PlayerMovementTutorial.cs
+++ Networking Mechanics/Assets/Scripts/Tutorials/JoystickMovement/PlayerMovementTutorial.cs
@@ -4,28 +4,21 @@ using UnityEngine;
 using Mirror;
 using UnityEngine.UI;
 
-//move player, let player use interact button
-//integrate pickup items script
-//if collided item is an object, pick up by pressing the button
-//do the check if inventory is full when pressed the button
-//if inventory full, debug log
-//if inventory empty, then pick up and instantiate the respective icon
+
 public class PlayerMovementTutorial : MonoBehaviour
 {
     private Rigidbody myBody;
-    public float moveForce = 25f;
-
-    public static Vector3 playerPos; //position of player
+    public float moveSpeed = 10f;
 
     [SerializeField] private FixedJoystick joystick = null;
 
-    private PlayerAnimation anim;
+    private Animator anim;
 
 
     void Start()
     {
         myBody = GetComponent<Rigidbody>();
-        anim = gameObject.GetComponent<PlayerAnimation>();
+        anim = GetComponent<Animator>();
 
     }
 
@@ -36,32 +29,46 @@ public class PlayerMovementTutorial : MonoBehaviour
 
     }
 
-    void Move()
+    private void Move()
     {
-        //playerPos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y - 2, gameObject.transform.position.z);
 
         //MOVE CHARACTER
 
-        myBody.velocity = new Vector3(joystick.Horizontal * moveForce, myBody.velocity.y, joystick.Vertical * moveForce); //x, y, z
+        myBody.velocity = new Vector3(joystick.Horizontal * moveSpeed,
+            myBody.velocity.y, joystick.Vertical * moveSpeed); //x, y, z
 
         //print(joystick.Horizontal);
 
         //Pressing the joystick at one value at least
         if (joystick.Horizontal != 0f || joystick.Vertical != 0f)
         {
-            anim.Move(0.6f); //Run state
+            BlendRun(0.6f); //Run state
+            NormalRun(true); //Normal run state
 
-            // Debug.Log("Joystick Horizontal: " + joystick.Horizontal);
-            // Debug.Log("Joystick Vertical: " + joystick.Vertical);
-
-            //Makes gameobject look towards direction specified, in this case, the movement direction of the player
+            //Makes gameobject look towards direction specified, 
+            //in this case, the movement direction of the player
             transform.rotation = Quaternion.LookRotation(myBody.velocity);
         }
 
         else
         {
-            anim.Move(0f); //Idle state   
+            BlendRun(0f); //Idle state   
+            NormalRun(false); //Normal run state
         }
+    }
+
+    //function to make player run (blend tree)
+    public void BlendRun(float blend)
+    {
+        anim.SetFloat("Blend", blend);
+    }
+
+    
+
+    //function make player run (default animation state)
+    public void NormalRun(bool run)
+    {
+        anim.SetBool("run", run);
     }
 
 
